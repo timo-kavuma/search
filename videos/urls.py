50cbc43f0ddc80b69fa61a73153252634a63be0d
Timo Kavuma <ptah@Dhjuty>
2020-06-03 14:18:03 +0300

    Visual Remix

---
+++ videos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from videos import views
+
+urlpatterns = [
+
+    # The home page
+    path('', views.index, name='home'),
+    # The single page
+    path('<slug:slug>', views.single, name='video_detail'),
+    # The tags page
+    path('tags', views.tags, name='tags'),
+    # The search page
+    path('search', views.search, name='search'),
+]
--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from videos.models import Video
 
 # Create your views here.
+def index(request):
+    #videos_list = Video.objects.all()
+    #return render(request, "videos/index.html", {'posts': videos_list})
+    return render(request, "videos/index.html")
+
+def single(request, slug):
+    #post = get_object_or_404(Video, slug = slug)
+    #return render(request, "videos/single.html", {'post': post})
+    return render(request, "videos/single.html")
+
+def tags(request):
+    #tags_list = Video.objects.all()
+    #return render(request, "videos/tags.html", {'posts': tags_list})
+    return render(request, "videos/tags.html")
+
+def search(request):
+    #search_list = Video.objects.all()
+    #return render(request, "videos/search.html", {'posts': search_list})
+    return render(request, "videos/search.html")
+
